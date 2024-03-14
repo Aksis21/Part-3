d92b46a9e69db0053a9d67f127b2542c2fff20e9
Aksis21 <james22@sheridancollege.ca>
2024-03-14 16:02:47 -0400

    Task 4b complete. Character selected is displayed in top right corner.

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,10 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI charText;
+    public static CharacterControl instance;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -14,6 +23,7 @@ public class CharacterControl : MonoBehaviour
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+
+        instance.charText.text = villager.ToString();
     }
-    
 }
